d5aca7bf89bf9cd151a57ef2fdc8ba79f0e2f308
Greg Taschuk <gregorytaschuk@gmail.com>
2018-11-02 12:51:27 -0400

    fix port used in env

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func main() {
 	var err error
 
 	environment := flag.String("e", "development", "Specify an environment {development, docker, infura}")
-	port := flag.String("p", "8080", "Specify a port for the gin server")
+	port := os.Getenv("PORT")
 	service := flag.String("s", "reveal", "Mode should be 'rest' or 'reveal' to indicate whether this is a REST api that accepts delegated reveals, or a 'revealer' service that reveals votes made to a voting contract")
 
 	flag.Usage = func() {
@@ -68,29 +68,27 @@ func main() {
 
 	// Uncomment to create some test data
 	//database.SetupTestData()
-	// Run a REST server to serve TruSet API requests
-	// Do this before processing our backlog, otherwise Heroku thinks we are taking too long to start
-	r := SetupRouter()
-	go r.Run(":" + *port) // listen and serve on 0.0.0.0:8080 by default
 
-	// Read data from all past events and reveal any we have not already revealed.
-	// This allows us to catch up automatically on lost logs whenever we restart.
-	// (If we never restart there's no extra work to do. But if we do, this is safer than ignoring the gap.)
-	// There are more efficient ways of doing this but they are more work. We would need to track
-	// logs that have been processed successfully.
 	commitRevealVotingContractAddress := env.GetString("commitRevealVotingContractAddress")
 	events.Init(clientString, commitRevealVotingContractAddress)
 	log.Printf("Listening to CRV contract at %v", commitRevealVotingContractAddress)
 
 	switch *service {
 	case "reveal":
+		// Read data from all past events and reveal any we have not already revealed.
+		// This allows us to catch up automatically on lost logs whenever we restart.
+		// (If we never restart there's no extra work to do. But if we do, this is safer than ignoring the gap.)
+		// There are more efficient ways of doing this but they are more work. We would need to track
+		// logs that have been processed successfully.
+
 		// poll for contract events that result in reveals
 		events.ProcessPastEvents()
 		events.ProcessFutureEvents()
 	case "api":
 		// Run a REST server to serve TruSet API requests
+		// Run a REST server to serve TruSet API requests
 		r := SetupRouter()
-		r.Run(":" + *port) // listen and serve on 0.0.0.0:8080 by default
+		r.Run(":" + port) // listen and serve on 0.0.0.0:8080 by default
 	default:
 		log.Fatal("-s option is not one of the supported service types - should be 'reveal' or 'api'")
 	}
